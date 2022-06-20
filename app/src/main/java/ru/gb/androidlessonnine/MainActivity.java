fa218efd343ff93137f453302f4d646e187c6119
Denis S <ruimei@vivaldi.net>
2022-06-20 18:21:58 +0300

    Done!

--- app/src/main/java/ru/gb/androidlessonnine/MainActivity.java
+++ app/src/main/java/ru/gb/androidlessonnine/MainActivity.java
@@ -1,14 +1,150 @@
 package ru.gb.androidlessonnine;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String NOTES = "notes";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] countries = getResources().getStringArray(R.array.country_names);
+        String[] capitals = getResources().getStringArray(R.array.country_capital);
+        ArrayList<Note> notes = Note.initArrayNote(capitals, countries);
+
+        InitialScreen initialScreen = new InitialScreen();
+        Bundle bundle = new Bundle();
+        bundle.putParcelableArrayList(NOTES, notes);
+        initialScreen.setArguments(bundle);
+
+        initToolbarAndDrawer();
+
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, initialScreen)
+                    .commit();
+        }
     }
+
+    private void initToolbarAndDrawer() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        initDrawer(toolbar);
+    }
+
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_about:
+                openAboutFragment();
+                return true;
+            case R.id.action_exit:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void initDrawer(Toolbar toolbar) {
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            switch (id) {
+                case R.id.action_drawer_about:
+                    openAboutFragment();
+                    return true;
+                case R.id.action_drawer_exit:
+                    showAlertDialog();
+                    return true;
+            }
+            return false;
+        });
+
+
+        navigationView.setNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.action_drawer_about:
+                    openAboutFragment();
+                    drawer.close();
+                    return true;
+                case R.id.action_drawer_exit:
+                    showAlertDialog();
+                    return true;
+            }
+            return false;
+        });
+
+    }
+
+    private void openAboutFragment() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .addToBackStack("")
+                .add(R.id.fragment_container, new AboutFragment()).commit();
+    }
+
+    private void showAlertDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("Внимание!")
+                .setMessage("Вы хотите выйти из приложения?")
+                .setPositiveButton("Да", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Завершение работы",
+                                Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                })
+                .setNegativeButton("Нет", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Продолжение работы",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNeutralButton(R.string.cancel, null)
+                .show();
+    }
+
+
 }
